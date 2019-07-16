671f0a88a829f246ab2bb963c018c2d7f6e70bba
Varun Kumar <varunkumaranalysis@gmail.com>
2019-07-16 17:24:34 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -3,13 +3,16 @@ class Person
     def name=(new_name)
         @name = new_name
     end
+
     def job=(new_job)
         @job = new_job
     end
+
     #getter
     def name
         @name = name
     end
+
     def job
         @job =job
     end
